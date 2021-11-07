1812cc4c37fb80c57255d04eb7ab81d8a674a802
Ivanilson-Costa18 <56151958+Ivanilson-Costa18@users.noreply.github.com>
2021-11-07 20:23:44 +0000

    update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,13 +1,13 @@
 module.exports = (discord, client, message) => {
-    if(/(https:\/\/)?(discord.gg\/)+[a-zA-Z0-9]/.test(message.content) && !message.member.roles.cache.some(r => r.name.toLowerCase() === "admin" )){
-        message.delete()    
-        let embed = new discord.MessageEmbed()
-        .setColor('#FF8400')
-        .setTitle('Warning: Invite Link')
-        .setDescription(`<@${message.author.id}>, you do not have permission to send invite links.`)
-        message.channel.send(embed)
-        return;
-    }
+    // if(/(https:\/\/)?(discord.gg\/)+[a-zA-Z0-9]/.test(message.content) && !message.member.roles.cache.some(r => r.name.toLowerCase() === "admin" )){
+    //     message.delete()    
+    //     let embed = new discord.MessageEmbed()
+    //     .setColor('#FF8400')
+    //     .setTitle('Warning: Invite Link')
+    //     .setDescription(`<@${message.author.id}>, you do not have permission to send invite links.`)
+    //     message.channel.send(embed)
+    //     return;
+    // }
     
     const PREFIX = "$"
     if(!message.content.startsWith(PREFIX) || message.author.bot) return;
