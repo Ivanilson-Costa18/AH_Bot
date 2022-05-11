dc6e4a41b0e3b728cf301df91afe366ef3fb1217
Ivanilson Gros <ivanilsongros@admins-MacBook-Pro.local>
2022-05-11 14:12:16 +0100

    play fix

--- commands/miscellaneous/ping.js
+++ commands/miscellaneous/ping.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'ping',
     description: 'check for connection',
     async execute(client, message, args, discord, cmd){
-        message.channel.send(`<@${message.author.id}, Pong!`)
+        message.channel.send(`<@${message.author.id}>, Pong!`)
     }
 }
